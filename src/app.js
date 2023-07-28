87bab504019258f232254ca6ecc622b5bb7d6965
user1fernan <fernandamedinajara2@gmail.com>
2023-07-28 13:58:31 -0400

    generador de dominios completado, condicional añadido

--- src/app.js
+++ src/app.js
@@ -2,10 +2,61 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#domname-gen").innerHTML = domainGenerator();
+  });
+  console.log("Hi Sebas and Luis");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let domainGenerator = () => {
+  let pron = ["the", "my", "your", "their", "our", "his", "her"];
+
+  let adj = [
+    "last",
+    "cute",
+    "small",
+    "tasty",
+    "amazing",
+    "salty",
+    "sugary",
+    "heavy",
+    "happy",
+    "sad",
+    "green",
+    "red"
+  ];
+
+  let noun = [
+    "of",
+    "rock",
+    "lamp",
+    "chair",
+    "wig",
+    "apple",
+    "leaf",
+    "cannoli",
+    "bolognesa"
+  ];
+
+  let extension = [".us", ".com", ".cl", ".net", ".io"];
+
+  let pronIndex = Math.floor(Math.random() * pron.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let extensionIndex = Math.floor(Math.random() * extension.length);
+
+  if (pron[pronIndex] == "the") {
+    return pron[0] + "" + adj[0] + "" + noun[0] + extension[0];
+  } else {
+    return (
+      pron[pronIndex] +
+      "" +
+      adj[adjIndex] +
+      "" +
+      noun[nounIndex] +
+      "" +
+      extension[extensionIndex]
+    );
+  }
 };
